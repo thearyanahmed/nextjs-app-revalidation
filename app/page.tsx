d99edd68b3986d23cb6a7f41172dda5d95a8658a
Aryan Ahmed <thearyanahmed@gmail.com>
2024-10-13 20:34:26 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Hello World
+     <p>Hello World</p>
+      <p>Hard coded now:  {new Date().toLocaleTimeString()}</p>
     </main>
   );
 }
