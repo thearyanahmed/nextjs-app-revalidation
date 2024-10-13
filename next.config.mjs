28a795c4474b5e042b24b037a36c65d3ae7e41ae
Aryan Ahmed <thearyanahmed@gmail.com>
2024-10-13 20:39:32 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,30 +2,30 @@ import { resolve } from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers: async () => {
-    return [
-      {
-        source: "/(.*?)",
-        headers: [
-          {
-            key: 'CDN-Cache-Control',
-            value: 'private, no-cache, no-store, max-age=0, must-revalidate',
-            // value: 's-maxage=0, stale-while-revalidate, stale-if-error',
-          },
-        ],
-      },
-      {
-        source: "/(.*?)",
-        headers: [
-          {
-            key: 'Cloudflare-CDN-Cache-Control',
-            value: 'private, no-cache, no-store, max-age=0, must-revalidate',
-            // value: 's-maxage=0, stale-while-revalidate, stale-if-error',
-          },
-        ],
-      },
-    ];
-  },
+  // headers: async () => {
+  //   return [
+  //     {
+  //       source: "/(.*?)",
+  //       headers: [
+  //         {
+  //           key: 'CDN-Cache-Control',
+  //           value: 'private, no-cache, no-store, max-age=0, must-revalidate',
+  //           // value: 's-maxage=0, stale-while-revalidate, stale-if-error',
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       source: "/(.*?)",
+  //       headers: [
+  //         {
+  //           key: 'Cloudflare-CDN-Cache-Control',
+  //           value: 'private, no-cache, no-store, max-age=0, must-revalidate',
+  //           // value: 's-maxage=0, stale-while-revalidate, stale-if-error',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   cacheHandler: resolve('./cache-handler.js'),
   cacheMaxMemorySize: 0, // disable default in-memory caching
 };
